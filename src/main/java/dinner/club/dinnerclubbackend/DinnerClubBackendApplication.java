76ab28cad52b8e4f33ebf375447804a3b96bbdfb
Tamara <tamara.misic119@gmail.com>
2025-04-11 18:44:13 +0200

    initial commit

---
+++ src/main/java/dinner/club/dinnerclubbackend/DinnerClubBackendApplication.java
@@ -0,0 +1,13 @@
+package dinner.club.dinnerclubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerClubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinnerClubBackendApplication.class, args);
+    }
+
+}
